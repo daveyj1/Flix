16f8d8847710158474719f844f4a9e0aca536683
daveyj <daveyj@purdue.edu>
2018-01-18 19:57:06 -0500

    Added detail screen to movie

---
+++ Flix/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  Flix
+//
+//  Created by Joseph Davey on 1/18/18.
+//  Copyright © 2018 Joseph Davey. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var backdropImageVIew: UIImageView!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    var movie: [String : Any]?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        posterImageView.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        posterImageView.layer.borderWidth = 2
+        
+        if let movie = movie {
+            titleLabel.text = movie["title"] as? String
+            releaseDateLabel.text = movie["release_date"] as? String
+            overviewLabel.text = movie["overview"] as? String
+            
+            let backdropPathString = movie["backdrop_path"] as! String
+            let posterPathString = movie["poster_path"] as! String
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            
+            let backdropURL = URL(string : baseURLString + backdropPathString)!
+            backdropImageVIew.af_setImage(withURL: backdropURL)
+            let posterURL = URL(string: baseURLString + posterPathString)!
+            posterImageView.af_setImage(withURL: posterURL)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
